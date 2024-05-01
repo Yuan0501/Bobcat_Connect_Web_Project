6f4e814850c0934d69f6ffe2450f85072999a1c2
Prasanna Dittakavi <prasannadittakavi@gmail.com>
2024-05-01 00:21:03 -0500

    Revert "Add mealplan purchase functionality"
    This reverts commit a6c3dee35079ec80a901eff1eecd70a44c0d512d.

--- BobcatConWeb/BobcatConApp/urls.py
+++ BobcatConWeb/BobcatConApp/urls.py
@@ -12,8 +12,7 @@ urlpatterns = [
     path('textbook/search/', views.search_textbook, name='search_textbook'),
     path('checkout/', views.checkout, name='checkout'),
     path('finalize-purchase/', views.finalize_purchase, name='finalize_purchase'),
-    path('purchase_confirmation/', views.purchase_confirmation_view, name='purchase_confirmation'),
-    path('meal-plans/', views.meal_plans, name='meal_plans')
+    path('purchase_confirmation/', views.purchase_confirmation_view, name='purchase_confirmation')
 ]
 
 if settings.DEBUG:
